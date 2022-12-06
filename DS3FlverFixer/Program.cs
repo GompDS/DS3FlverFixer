f1076ad8793db477093162947424c69dabc538d9
GompDS <79070939+GompDS@users.noreply.github.com>
2022-12-06 00:11:00 -0800

    Supports bnd4 with multiple flvers

--- DS3FlverFixer/Program.cs
+++ DS3FlverFixer/Program.cs
@@ -10,54 +10,51 @@ internal static class Program
     {
         if (!BND4.Is(args[0])) return;
         BND4 bnd = BND4.Read(args[0]);
-
-        BinderFile? file = bnd.Files.FirstOrDefault(x => x.Name.Contains(".flver"));
-        if (file == null)
-        {
-            throw new FileNotFoundException("Binder contains no flver!");
-        }
-
-        FLVER2 flver = FLVER2.Read(file.Bytes);
-        flver.BufferLayouts = new List<FLVER2.BufferLayout>();
-        flver.GXLists = new List<FLVER2.GXList>();
         
-        string cwd = AppDomain.CurrentDomain.BaseDirectory;
-
-        FLVER2MaterialInfoBank materialInfoBank = FLVER2MaterialInfoBank.ReadFromXML($"{cwd}Res\\BankDS3.xml");
-        
-        List<FLVER2.Material> distinctMaterials = flver.Materials.DistinctBy(x => x.MTD).ToList();
-        foreach (var distinctMat in distinctMaterials)
+        foreach (BinderFile flverFile in bnd.Files.Where(x => x.Name.EndsWith(".flver", StringComparison.OrdinalIgnoreCase)))
         {
-            FLVER2.GXList gxList = new();
-            gxList.AddRange(materialInfoBank
-                .GetDefaultGXItemsForMTD(Path.GetFileName(distinctMat.MTD).ToLower()));
-
-            if (flver.IsNewGxList(gxList))
-            {
-                flver.GXLists.Add(gxList);
-            }
-			
-            foreach (var mat in flver.Materials.Where(x => x.MTD.Equals(distinctMat.MTD)))
-            {
-                mat.GXIndex = flver.GXLists.Count - 1;
-            }
-        }
-        
-        foreach (FLVER2.Mesh? mesh in flver.Meshes)
-        {
-            FLVER2MaterialInfoBank.MaterialDef matDef = materialInfoBank.MaterialDefs.Values
-                .First(x => x.MTD.Equals(
-                    $"{Path.GetFileName(flver.Materials[mesh.MaterialIndex].MTD).ToLower()}"));
-
-            List<FLVER2.BufferLayout> bufferLayouts = matDef.AcceptableVertexBufferDeclarations[0].Buffers;
+            FLVER2 flver = FLVER2.Read(flverFile.Bytes);
+            flver.BufferLayouts = new List<FLVER2.BufferLayout>();
+            flver.GXLists = new List<FLVER2.GXList>();
             
-            mesh.Vertices = mesh.Vertices.Select(x => x.Pad(bufferLayouts)).ToList();
-            List<int> layoutIndices = flver.GetLayoutIndices(bufferLayouts);
-            mesh.VertexBuffers = layoutIndices.Select(x => new FLVER2.VertexBuffer(x)).ToList();
+            string cwd = AppDomain.CurrentDomain.BaseDirectory;
+
+            FLVER2MaterialInfoBank materialInfoBank = FLVER2MaterialInfoBank.ReadFromXML($"{cwd}Res\\BankDS3.xml");
+            
+            List<FLVER2.Material> distinctMaterials = flver.Materials.DistinctBy(x => x.MTD).ToList();
+            foreach (var distinctMat in distinctMaterials)
+            {
+                FLVER2.GXList gxList = new();
+                gxList.AddRange(materialInfoBank
+                    .GetDefaultGXItemsForMTD(Path.GetFileName(distinctMat.MTD).ToLower()));
+
+                if (flver.IsNewGxList(gxList))
+                {
+                    flver.GXLists.Add(gxList);
+                }
+			    
+                foreach (var mat in flver.Materials.Where(x => x.MTD.Equals(distinctMat.MTD)))
+                {
+                    mat.GXIndex = flver.GXLists.Count - 1;
+                }
+            }
+            
+            foreach (FLVER2.Mesh? mesh in flver.Meshes)
+            {
+                FLVER2MaterialInfoBank.MaterialDef matDef = materialInfoBank.MaterialDefs.Values
+                    .First(x => x.MTD.Equals(
+                        $"{Path.GetFileName(flver.Materials[mesh.MaterialIndex].MTD).ToLower()}"));
+
+                List<FLVER2.BufferLayout> bufferLayouts = matDef.AcceptableVertexBufferDeclarations[0].Buffers;
+                
+                mesh.Vertices = mesh.Vertices.Select(x => x.Pad(bufferLayouts)).ToList();
+                List<int> layoutIndices = flver.GetLayoutIndices(bufferLayouts);
+                mesh.VertexBuffers = layoutIndices.Select(x => new FLVER2.VertexBuffer(x)).ToList();
+            }
+            
+            flverFile.Bytes = flver.Write();
         }
-        
-        file.Bytes = flver.Write();
-        
+            
         bnd.Write(args[0], DCX.Type.DCX_DFLT_10000_44_9);
     }
 }
